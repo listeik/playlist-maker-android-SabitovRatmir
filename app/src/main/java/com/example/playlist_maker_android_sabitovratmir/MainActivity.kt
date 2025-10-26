1a0c7ffeb95df46955646be25971045792e3ece2
SabitovRatmir <sabitovratmir@rambler.ru>
2025-10-26 14:23:29 +0300

    feat: complete sprint 3 tasks
    - feat: implement MainActivity layout according to Figma design
    - feat: add SettingsActivity and SearchActivity classes
    - feat: implement navigation between main, settings and search screens
    - chore: update AndroidManifest.xml with new activities
    - docs: update README.md with sprint 3 changes
    - docs: add sprint-3 checklist

---
+++ app/src/main/java/com/example/playlist_maker_android_sabitovratmir/MainActivity.kt
@@ -0,0 +1,194 @@
+package com.example.playlist_maker_android_sabitovratmir
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
+import com.example.playlist_maker_android_sabitovratmir.ui.theme.PlaylistmakerandroidSabitovRatmirTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PlaylistmakerandroidSabitovRatmirTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF3772E7))
+    ) {
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(70.dp)
+                .background(Color(0xFF3772E7)),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Text(
+                text = stringResource(R.string.playlist_maker),
+                color = Color.White,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.padding(start = 16.dp)
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+                .background(Color.White)
+                .padding(top = 8.dp)
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(0.dp)
+            ) {
+
+                MenuCard(
+                    iconRes = R.drawable.search,
+                    text = stringResource(R.string.search),
+                    onClick = {
+                        val intent = Intent(context, SearchActivity::class.java)
+                        context.startActivity(intent)
+                    }
+                )
+
+
+                MenuCard(
+                    iconRes = R.drawable.playlist,
+                    text = stringResource(R.string.playlist),
+                    onClick = {
+                        //пусто, так как в задании ведь не сказано создать класс
+                    }
+                )
+
+
+                MenuCard(
+                    iconRes = R.drawable.heart,
+                    text = stringResource(R.string.favorites),
+                    onClick = {
+                        //пусто, так как в задании ведь не сказано создать класс
+                    }
+                )
+                MenuCard(
+                    iconRes = R.drawable.settings,
+                    text = stringResource(R.string.settings),
+                    onClick = {
+                        val intent = Intent(context, SettingsActivity::class.java)
+                        context.startActivity(intent)
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MenuCard(
+    iconRes: Int,
+    text: String,
+    onClick: () -> Unit
+) {
+    Card(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 0.dp
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp, horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Icon(
+                painter = painterResource(id = iconRes),
+                contentDescription = text,
+                modifier = Modifier.size(19.dp),
+                tint = Color(0xFF1A1B22)
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+
+            Text(
+                text = text,
+                color = Color(0xFF1A1B22),
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.weight(1f)
+            )
+
+
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                contentDescription = "Перейти",
+                modifier = Modifier.size(24.dp),
+                tint = Color(0xFFAEAFB4)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MainScreenPreview() {
+    PlaylistmakerandroidSabitovRatmirTheme {
+        MainScreen()
+    }
+}
---
+++ app/src/main/java/com/example/playlist_maker_android_sabitovratmir/SearchActivity.kt
@@ -0,0 +1,33 @@
+package com.example.playlist_maker_android_sabitovratmir
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+class SearchActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Экран поиска",
+                    fontSize = 24.sp,
+                    color = Color.Black
+                )
+            }
+        }
+    }
+}
